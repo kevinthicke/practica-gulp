8ec743ed486a3043626ad2328d57263590c1080f
Kevin Cabrera <keviincabrera@gmail.com>
2018-10-21 13:33:38 +0100

    se añandió la función de minificar

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 let gulp = require('gulp');
 let less = require('gulp-less');
+let cleancss = require('gulp-clean-css');
+let rename = require('gulp-rename');
 
 gulp.task('css', () => {
     return gulp.src('src/css/*.less')
         .pipe(less())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(cleancss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('dist/css'))
 });
 
